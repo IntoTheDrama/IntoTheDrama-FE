4544a5fc6e415d9a7e3c19d67878a7c74493f0bc
juyeon Bae <w2316@e-mirim.hs.kr>
2024-10-15 10:33:17 +0900

    feat : playlist page 구현

---
+++ playlist/js/playlist.js
@@ -0,0 +1,14 @@
+const playlistBox = document.querySelector('.scroll-container'); // 상위 컨테이너 선택
+
+let ostHtml = ''
+dramaOst.forEach(ost => {
+    ostHtml += `
+        <article class="playList">
+            <div class="ostTitle">${ost.dramaTitle}</div>
+        </article>
+    `;
+
+    playlistBox.innerHTML = ostHtml;
+});
+
+
