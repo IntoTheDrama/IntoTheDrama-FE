5358368fed1a570f85641b6180db97c5a657d36c
juyeon Bae <w2316@e-mirim.hs.kr>
2024-10-20 23:04:56 +0900

    feat : Implement search function

--- timemachine/js/1990.js
+++ timemachine/js/1990.js
@@ -1,43 +1,61 @@
 const dramaPosterContainer90 = document.querySelector('.year90');
-let showPoster90 = ''
-dramaPoster1990.forEach(drama => {
-    showPoster90 += `
-    <div class="posterContainer">
-    <img src="/image/poster/${drama.posterImg}.png" alt="${drama.title}">
-    <p class="drama-title">${drama.title}</p>
-    </div>
-    `
-})
-dramaPosterContainer90.innerHTML = showPoster90;
+let showPoster90 = '';
 
-//드라마 포스터
-document.querySelectorAll('.dramaPoster-container img').forEach((poster, index) => {
-    poster.addEventListener('click', function() {
-        const selectedDrama = dramaPoster1990[index];
-
-        document.querySelector('.poster-img').src = `/image/poster/${selectedDrama.widthImg}.png`;
-        document.querySelector('.description h2').innerText = selectedDrama.title;
-        document.querySelector('.description p').innerHTML = selectedDrama["sub-title"];
-
-        // 출연진 정보 
-        const crewContainer = document.querySelector('.crew-grid-container');
-        crewContainer.innerHTML = '';  
-
-        // 배우 이미지, 이름, 직업
-        selectedDrama.actors.forEach((actor, i) => {
-             
-            crewContainer.innerHTML += `
-                <div class="crew-member">
-                    <img src="/image/people/${selectedDrama.actors[i]}.png" alt="${actor}">
-                    <div class="crew">
-                        <p class="name">${actor}</p>
-                        <p class="job">${selectedDrama.job[i]}</p> 
-                    </div>
-                </div>`;
-        });
-
-         
-        document.querySelector('#main').style.display = 'none';
-        document.querySelector('#main2').style.display = 'block';
+function renderDramas90(dramas) {
+    showPoster90 = ''; 
+    dramas.forEach(drama => {
+        showPoster90 += `
+        <div class="posterContainer">
+        <img src="/image/poster/${drama.posterImg}.png" alt="${drama.title}">
+        <p class="drama-title">${drama.title}</p>
+        </div>
+        `;
     });
+
+    dramaPosterContainer90.innerHTML = showPoster90;
+
+    
+    document.querySelectorAll('.posterContainer img').forEach((poster, index) => {
+        poster.addEventListener('click', function() {
+            const selectedDrama = dramas[index]; 
+
+            document.querySelector('.poster-img').src = `/image/poster/${selectedDrama.widthImg}.png`;
+            document.querySelector('.description h2').innerText = selectedDrama.title;
+            document.querySelector('.description p').innerHTML = selectedDrama["sub-title"];
+
+            // 출연진 
+            const crewContainer = document.querySelector('.crew-grid-container');
+            crewContainer.innerHTML = '';  
+
+            // 배우 이미지, 이름, 직업
+            selectedDrama.actors.forEach((actor, i) => {
+                crewContainer.innerHTML += `
+                    <div class="crew-member">
+                        <img src="/image/people/${selectedDrama.actors[i]}.png" alt="${actor}">
+                        <div class="crew">
+                            <p class="name">${actor}</p>
+                            <p class="job">${selectedDrama.job[i]}</p> 
+                        </div>
+                    </div>`;
+            });
+
+            document.querySelector('#main').style.display = 'none';
+            document.querySelector('#main2').style.display = 'block';
+        });
+    });
+}
+
+renderDramas90(dramaPoster1990);
+
+// 검색 기능 구현
+const searchBox = document.querySelector('.search-box');
+
+searchBox.addEventListener('input', function() {
+    const query = searchBox.value.toLowerCase(); 
+
+    const filteredDramas = dramaPoster1990.filter(drama =>
+        drama.title.toLowerCase().includes(query)
+    );
+
+    renderDramas90(filteredDramas);
 });
