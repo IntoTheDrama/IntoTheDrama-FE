bc58c2dbedce209625bb9b3a1dcbadc23c8404f0
juyeon Bae <w2316@e-mirim.hs.kr>
2024-10-20 18:00:33 +0900

    feat : 시대별 드라마 정보 페이지 구현

---
+++ timemachine/js/2000.js
@@ -0,0 +1,44 @@
+//2000년도
+const dramaPosterContainer2000 = document.querySelector('.year00');
+let showPoster2000 = '';
+
+dramaPoster2000.forEach(drama => {
+    showPoster2000 += `
+    <img src="/image/poster/${drama.posterImg}.png" alt="${drama.title}">
+    `;
+});
+
+dramaPosterContainer2000.innerHTML = showPoster2000;
+
+//드라마 포스터
+document.querySelectorAll('.dramaPoster-container img').forEach((poster, index) => {
+    poster.addEventListener('click', function() {
+        const selectedDrama = dramaPoster2000[index];
+
+        document.querySelector('.poster-img').src = `/image/poster/${selectedDrama.widthImg}.png`;
+        document.querySelector('.description h2').innerText = selectedDrama.title;
+        document.querySelector('.description p').innerHTML = selectedDrama["sub-title"];
+
+        // 출연진 정보 
+        const crewContainer = document.querySelector('.crew-grid-container');
+        crewContainer.innerHTML = '';  
+
+        // 배우 이미지, 이름, 직업
+        selectedDrama.actors.forEach((actor, i) => {
+             
+            crewContainer.innerHTML += `
+                <div class="crew-member">
+                    <img src="/image/people/${selectedDrama.actors[i]}.png" alt="${actor}">
+                    <div class="crew">
+                        <p class="name">${actor}</p>
+                        <p class="job">${selectedDrama.job[i]}</p> 
+                    </div>
+                </div>`;
+        });
+
+         
+        document.querySelector('#main').style.display = 'none';
+        document.querySelector('#main2').style.display = 'block';
+    });
+});
+
