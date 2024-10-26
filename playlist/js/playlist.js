896700069d7645315867ac193d71c3d61d0ad0ea
juyeon Bae <w2316@e-mirim.hs.kr>
2024-10-27 02:21:56 +0900

    fix : playlist 부분 반응형 다시 구현

--- playlist/js/playlist.js
+++ playlist/js/playlist.js
@@ -1,12 +1,14 @@
-const playlistBox = document.querySelector('.scroll-container');  
+const playlistBox = document.querySelector('.scroll-container');
+const audioPlayer = document.getElementById('audio-player');
+let currentPlaying = null; // 현재 재생 중인 음악을 추적하기 위한 변수
 
-let ostHtml = ''
+let ostHtml = '';
 dramaOst.forEach(ost => {
     ostHtml += `
-        <article class="playList">
-            <div class="ostTitle">${ost.dramaTitle}</div>
-        </article>
+        <div class="play-list" data-audio="${ost.audio}">${ost.dramaTitle}</div>
     `;
-
-    playlistBox.innerHTML = ostHtml;
 });
+
+playlistBox.innerHTML = ostHtml;
+
+
